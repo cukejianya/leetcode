2331236da5fb169f56c995f041ebc1bfaf6e6ce1
Chinedum Ukejianya <cukejianya@gmail.com>
2019-08-09 16:53:29 -0400

    Finish merge_two_sorted_lists.py

---
+++ linked_list/merge_two_sorted_lists.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoListsRecur(self, l1, l2):
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoListsRecur(l1.next, l2)
+            return l1
+        l2.next = self.mergeTwoListsRecur(l1, l2.next)
+        return l2
+
+    def mergeTwoLists(self, l1, l2):
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            head = l1
+            l1 = l1.next
+        else:
+            head = l2
+            l2 = l2.next
+        curr = head
+        while(l1 and l2):
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+
+        curr.next = l1 if l1 else l2
+
+        return head
+
+
