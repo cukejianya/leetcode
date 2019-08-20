7481e65bf795a7c28ce72f258af985cfecdae940
Chinedum Ukejianya <cukejianya@gmail.com>
2019-08-20 17:51:23 -0400

    Gave up on initial O(logn) attempt

--- array_and_strings/find_k_closest_elm.py
+++ array_and_strings/find_k_closest_elm.py
@@ -9,31 +9,27 @@ class Solution:
                 end = mid 
             mid = (end + start) // 2
         return mid
+    
+    def get_dist(self, arr, a, b):
+        return abs(arr[a] + -1*arr[b]) 
 
     def findClosestElements(self, arr, k, x):
         mid = self.find_x(arr, x)
         if k == 1:
             return [arr[mid]]
-        start = 0 if mid - k < 0 else mid - k
+        start = end = mid
         len_arr = len(arr)
-        end = len_arr if mid + k > len_arr - 1 else mid + k + 1
-        left = right = mid
-        delta = right - left + 1
-        print(start, left, mid, right, end)
-        while (start < left) and (right < end):
-            print('Begin:', start, left, mid, right, end)
-            if abs(arr[mid] + -1 * arr[left]) > abs(arr[mid] + -1*arr[right]):
-                start = mid - k + right
-                start =  0 if start < 0 else start
-                right = (right + end) // 2 
+
+        for i in range(k):
+            if start == 0:
+                end = k
+                break
+            if end == len_arr:
+                start = end - k 
+                break
+            if self.get_dist(arr, start, mid) > self.get_dist(arr, end, mid):
+                end += 1
             else:
-                end = mid - k - start + 1
-                end = len_arr if end  > len_arr else end
-                left = (left + start) // 2
-            print('End:', start, left, mid, right, end)
+                start -= 1
+
         return arr[start:end]
-
-print(Solution().findClosestElements([1,2,3,4,5], 3, 4))
-
-
-
