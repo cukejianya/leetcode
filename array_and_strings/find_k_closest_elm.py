d137230a9ae5cf69a19d0002cea5941acd84ceb7
Chinedum Ukejianya <cukejianya@gmail.com>
2019-08-20 22:23:29 -0400

    Try windowing approach for find_k_closest_elm.py

--- array_and_strings/find_k_closest_elm.py
+++ array_and_strings/find_k_closest_elm.py
@@ -1,33 +1,26 @@
 class Solution:
-    def find_x(self, arr, x):
-        start, end = 0, len(arr) - 1
-        mid = end
-        while start < end:
-            if arr[mid] < x:
-                start = mid + 1
-            else:
-                end = mid 
-            mid = (end + start) // 2
-        return mid
-
     def findClosestElements(self, arr, k, x):
-        start = end = self.find_x(arr, x)
-        if k == 1:
-            return [arr[start]]            
+        start = 0
+        end = k
+        prev_sum = 2**33
+        arr_sum = sum(arr[start:end])
         len_arr = len(arr)
-        start = start - 1 if start > 0 and abs(arr[start] - x) > abs(arr[end + 1] - x) else start
-        end = end + 1 if start == end and end < len_arr else end
-        while k > end - start:
-            if start <= 0:
-                start = 0
-                end = k
-                break
-            if end + 1 >= len_arr:
-                end = len_arr
-                start = end - k 
-                break
-            if abs(arr[start - 1] - x) > abs(arr[end + 1] - x):
-                end += 1
-            else:
+        print(arr[0:k])
+        while(abs(prev_sum / k - x) >= abs(arr_sum / k - x)) and end < len_arr:
+            if (abs(prev_sum/k - x) == abs(arr_sum/k - x) and 
+                    arr[start - 1] < arr[start]):
                 start -= 1
-        return arr[start:end]
+                end -= 1
+                break
+            print(prev_sum, abs(arr_sum / k - x), abs(arr_sum / k - x), arr_sum)
+            print(arr[start:end])
+            prev_sum = arr_sum
+            start += 1
+            arr_sum  = prev_sum - arr[start] + arr[end]
+            end += 1
+        return arr[start:end] 
+
+print(Solution().findClosestElements([0,0,0,1,3,5,6,7,8,8],2,2), '\n')
+#print(Solution().findClosestElements([1,2,3,4,5],4,3),'\n')
+#print(Solution().findClosestElements([0,0,1,2,3,3,4,7,7,8],3,5),'\n')
+#print(Solution().findClosestElements([0,1,1,1,2,3,6,7,8,9],9,4),'\n')
