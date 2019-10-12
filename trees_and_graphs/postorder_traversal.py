213342028809146b4966b439fb43439b4645e165
Chinedum Ukejianya <cukejianya@gmail.com>
2019-10-12 14:38:20 -0400

    Finish postorder_traversal.py (recursively)

---
+++ trees_and_graphs/postorder_traversal.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def postorderTraversal(self, root):
+        if not root:
+            return []
+        left = self.postorderTraversal(root.left)
+        right = self.postorderTraversal(root.right)
+        return left + right + [root.val]
