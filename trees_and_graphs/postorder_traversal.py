10ee9999533c62115cebb17562d02e92fd572b1c
Chinedum Ukejianya <cukejianya@gmail.com>
2019-10-12 15:21:39 -0400

    Finish postorder_traversal.py (iteratively)

--- trees_and_graphs/postorder_traversal.py
+++ trees_and_graphs/postorder_traversal.py
@@ -9,6 +9,19 @@ class Solution:
     def postorderTraversal(self, root):
         if not root:
             return []
-        left = self.postorderTraversal(root.left)
-        right = self.postorderTraversal(root.right)
-        return left + right + [root.val]
+        stack = [root]
+        visited = set([None])
+        values = []
+        while(stack):
+            curr = stack.pop()
+            if curr.left in visited and curr.right in visited:
+                values.append(curr.val)
+            if curr.left not in visited:
+                stack.append(curr)
+                stack.append(curr.left)
+                visited.add(curr.left)
+            elif curr.right not in visited:
+                stack.append(curr)
+                stack.append(curr.right)
+                visited.add(curr.right)
+        return values
